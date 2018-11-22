23f9cb80d7036e94684ba53314868fb9618b60aa
littlegrace <dongni.wx@alibaba-inc.com>
2018-11-22 17:21:03 +0800

    tutorial01

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Module1 from './module/module1'
-import Module2 from './module/module2'
-import './style/index.css'
+// import Module1 from './module/module1'
+// import Module2 from './module/module2'
+// import './style/index.css'
+import Game from './tutorial01/index';
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Module1 />
-                <Module2 />
-            </div>
-        )
-    }
-}
+// export default class App extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Module1 />
+//                 <Module2 />
+//             </div>
+//         )
+//     }
+// }
 
 
 render(
-    <App />,
+    <Game />,
     document.getElementById('app')
 );
